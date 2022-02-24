be7a28ea3833f3b772073c58559bf7d6de0a3206
mmaider <moiseevasd@gmail.com>
2022-02-24 09:16:48 +0300

    Наследование

--- src/Point2d.java
+++ src/Point2d.java
@@ -1,29 +1,33 @@
+// класс точки в двумерном пространстве
 public class Point2d {
-    private double xCoord;
-    private double yCoord;
-
-    public Point2d(double x, double y) {
+    //координата X
+    protected double xCoord;
+    // координата Y
+    protected double yCoord;
+    // Конструктор инициализации
+    public Point2d ( double x, double y) {
         xCoord = x;
         yCoord = y;
     }
-
-    public Point2d() {
+    // Конструктор объявления.
+    public Point2d () {
+        //заполняет поля нулевыми значениями
         this(0, 0);
     }
-
-    public double getX() {
+    // вызов координаты X
+    public double getX () {
         return xCoord;
     }
-
-    public double getY() {
+    // вызов координаты Y
+    public double getY () {
         return yCoord;
     }
-
-    public void setX(double val) {
+    // установка значения координаты X.
+    public void setX ( double val) {
         xCoord = val;
     }
-
-    public void setY(double val) {
+    //  установка значения координаты Y.
+    public void setY ( double val) {
         yCoord = val;
     }
 }
